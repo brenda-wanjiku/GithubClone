bb92fc2dd83a6ace19defbe9722ce7e61ac9fd3b
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-04-14 08:55:29 +0300

    Adds copy of key to environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '4c9bd107a200d0fde0c69f3205d3bdf3d93e1641',
+  apiUser: 'http://api.github.com/users?access_token=4c9bd107a200d0fde0c69f3205d3bdf3d93e1641'
 };
