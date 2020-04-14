240a1056e489dc6adfb641cbd77d46e5832c6271
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-04-14 09:12:00 +0300

    Edits the mouse hover direcive

--- src/app/github.directive.ts
+++ src/app/github.directive.ts
@@ -1,12 +1,21 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appGithub]'
 })
 export class GithubDirective {
+  constructor(private el: ElementRef) { }
 
-  constructor(private elem:ElementRef){
-    this.elem.nativeElement.style.backgroundColor= 'yellow';
+  private highlight(color: string){
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+  @HostListener('mouseenter') onMouseEnter(){
+    this.highlight('yellow')
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.highlight(null)
   }
 
 }
